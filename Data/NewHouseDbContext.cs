12c1d4a4cb82ffd445629ad2d6c34d2dabc1ae62
HristoFanev <hristo2204@abv.bg>
2023-03-02 08:28:05 +0200

    Controllers

---
+++ Data/NewHouseDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewHouse.Data
+{
+    public class NewHouseDbContext : IdentityDbContext<User>
+    {
+        public NewHouseDbContext(DbContextOptions<NewHouseDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<Type> Types { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
---
+++ Data/Property.cs
@@ -0,0 +1,18 @@
+﻿namespace NewHouse.Data
+{
+    public enum StatusType { busy, free } //busy = 1, free = 2
+    public class Property
+    {
+        public int Id { get; set; }
+        public int Floor { get; set; }
+        public double Quadrature { get; set; }
+        public StatusType Status { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public DateTime RegisterOn { get; set; }
+        public int TypeId { get; set; }
+        public Type Type { get; set; }
+        public ICollection<Reservation> Reservations { get; set; }
+    }
+}
