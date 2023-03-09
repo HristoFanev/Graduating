5a9cfd61753c7a0c1f972c395d9c1ff60e829313
TanyaAleksieva <hristo2204@abv.bg>
2023-03-09 16:05:55 +0200

    Adding supper admin

--- NewHouse/Program.cs
+++ NewHouse/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NewHouse.Data;
+using NewHouse.Service;
 
 namespace NewHouse
 {
@@ -40,6 +41,7 @@ namespace NewHouse
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.PrepareDataBase().Wait();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
